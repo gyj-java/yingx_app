eb5696f51ffd26c30c833b4035640e80ac9d8fdf
gyj-java <59904269+gyj-java@users.noreply.github.com>
2020-04-14 11:55:12 +0800

    Update AliyunSendShortMessage.java

--- src/main/java/com/gyj/yingx/util/AliyunSendShortMessage.java
+++ src/main/java/com/gyj/yingx/util/AliyunSendShortMessage.java
@@ -33,8 +33,8 @@ public class AliyunSendShortMessage {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 //替换成你的AK
-        final String accessKeyId = "LTAI4FkCMSb1Aik5tgViAPRT";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "XkJGvzX9UHaEpft02cboSH7crDfXdJ";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "******";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "*********";//你的accessKeySecret，参考本文档步骤2
 //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-beijing", accessKeyId,
                 accessKeySecret);
