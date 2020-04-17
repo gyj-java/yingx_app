793f681f61223d8d195547d4a5a8d9cc84a13d99
gyj-java <59904269+gyj-java@users.noreply.github.com>
2020-04-17 16:53:41 +0800

    Update YingxAppApplicationTests.java

--- src/test/java/com/gyj/yingx/YingxAppApplicationTests.java
+++ src/test/java/com/gyj/yingx/YingxAppApplicationTests.java
@@ -67,8 +67,8 @@ public class YingxAppApplicationTests {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-beijing.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4Fft2pfxzwE2P2qLez3N";
-        String accessKeySecret = "vfpqevOMfwxGPEX7PRSJ9T5a7FoZof";
+        String accessKeyId = "*****";
+        String accessKeySecret = "*****";
 
         String bucket="yingx-186";
 
